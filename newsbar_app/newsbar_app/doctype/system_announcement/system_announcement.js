619664f0d4c27fb95e2f28b21183236a6a539607
Khaled Saad <dev.khaled.saad@gmail.com>
2026-09-20 06:09:26 +0000

    refactor: convert doctype from fixtures to native app doctype

---
+++ newsbar_app/newsbar_app/doctype/system_announcement/system_announcement.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, khaled and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("System Announcement", {
+// 	refresh(frm) {
+
+// 	},
+// });
